53d566bea6e395fab5e33b00314e95bb6e6b9389
Robert Gordon <96712943+bobbygrdn@users.noreply.github.com>
2025-01-30 22:33:02 -0800

    updates url for menu scheduler app

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const projects = [
       "https://images.unsplash.com/photo-1590479773265-7464e5d48118?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     status: "In Development",
     tech: ["React", "TypeScript", "Supabase", "PostgreSQL"],
-    link: "https://v53-tier3-team-38.netlify.app/",
+    link: "https://github.com/chingu-voyages/V53-tier3-team-38",
   },
   {
     id: 2,
