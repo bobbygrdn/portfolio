e13cbfb670fbf9594add59f5d95151a470399631
bobbygrdn <bobbygrdn777@gmail.com>
2025-06-17 22:00:56 -0700

    feat: integrate VitePWA plugin for enhanced PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Bobbygrdn Portfolio',
+        short_name: 'Portfolio',
+        start_url: '/portfolio/',
+        display: 'standalone',
+        background_color: '#09090b',
+        theme_color: '#38bdf8',
+        icons: [
+          {
+            src: 'assets/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
