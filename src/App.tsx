42e555a7ba755467404fc3284efb1e9b537303c8
bobbygrdn <bobbygrdn777@gmail.com>
2025-06-17 21:50:15 -0700

    refactor: update project images to use local assets and remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
-  Github,
   Mail,
-  Linkedin,
   ExternalLink,
   Code2,
   Boxes,
@@ -23,7 +21,7 @@ const projects = [
     description:
       "This project aims to develop a comprehensive menu scheduling application that enables managers to create and share weekly menus with their staff. The application ensures that employees have clear visibility of the planned meals while accommodating specific dietary restrictions and allergies.",
     image:
-      "https://github.com/user-attachments/assets/8aadacfe-1e57-4a1e-b6c6-12a9ddb8ff83",
+      "/assets/418780902-8aadacfe-1e57-4a1e-b6c6-12a9ddb8ff83.png",
     status: "Live",
     tech: ["React", "TypeScript", "Supabase", "PostgreSQL"],
     link: "https://v53-tier3-team-38.netlify.app/",
@@ -36,7 +34,7 @@ const projects = [
     description:
       "Intracom is an open-source communication tool developed by engineers, for engineers. Collaborate on real-world projects, showcase your contributions, and get noticed by recruitersy a global community of engineers.",
     image:
-      "https://intracom.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fintracom-reaction.d9cd3d6d.png&w=1920&q=75",
+      "/assets/intracom-reaction.webp",
     status: "In Development",
     tech: ["React", "TypeScript", "TailwindCSS", "Electron.js", "Socket.io", "Express.js", "MongoDB"],
     link: "https://intracom.app/",
@@ -49,7 +47,7 @@ const projects = [
     description:
       "My goal was to create a comprehensive, user-friendly learning platform that empowers users to master web development skills through interactive lessons, quizzes, and hands-on projects. The platform features robust content management, progress tracking, and assessment tools, along with secure user authentication and authorization to ensure safe access.",
     image:
-      "https://private-user-images.githubusercontent.com/96712943/254004702-d2e18893-8fe5-4bc5-a697-9a0c4249af65.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDEwNDc5MzEsIm5iZiI6MTc0MTA0NzYzMSwicGF0aCI6Ii85NjcxMjk0My8yNTQwMDQ3MDItZDJlMTg4OTMtOGZlNS00YmM1LWE2OTctOWEwYzQyNDlhZjY1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAzMDQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMzA0VDAwMjAzMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNhZmMyN2RlNDA3ZGEyN2JmZmM0ZmFhY2M4ZWMyYTI5MGIxYjc3NGQxMGE2YzNiODUzNDA1MjUyMzdkZTE5YTQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.N7TiVyB0q_OFw9p881CVoZ0WYR_EavG6Kr5_39ghK9k",
+      "/assets/254004702-d2e18893-8fe5-4bc5-a697-9a0c4249af65.png",
     status: "Completed",
     tech: ["React", "Java", "Spring Boot", "MySQL"],
     link: "https://github.com/bobbygrdn/learning_platform",
@@ -62,35 +60,19 @@ const projects = [
     description:
       "A Ticket Management System designed to efficiently track and manage maintenance requests across company buildings. Built with React, Node.js, and PostgreSQL for a robust and scalable solution.",
     image:
-      "https://camo.githubusercontent.com/fab4c6220d9f638d7f364a6618aa2ab9b38012fdcb1205b55c6461a686f24f70/68747470733a2f2f64377674653276396c346471642e636c6f756466726f6e742e6e65742f74656368706f72745f6c6f67696e706167652e6a7067",
+      "/assets/68747470733a2f2f64377674653276396c346471642e636c6f756466726f6e742e6e65742f74656368706f72745f6c6f67696e706167652e6a7067.jpeg",
     status: "Complete",
     tech: ["React", "Node.js", "express.js", "PostgreSQL"],
     link: "https://github.com/Team-Cache-Out/tech-port",
   },
 ];
 export function App() {
-  const [activeSection, setActiveSection] = useState("");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isImageLoading, setIsImageLoading] = useState<{
     [key: number]: boolean;
   }>({});
   const [showFloatingCTA, setShowFloatingCTA] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = ["projects", "skills", "contact"];
-      const currentSection = sections.find((section) => {
-        const element = document.getElementById(section);
-        if (element) {
-          const rect = element.getBoundingClientRect();
-          return rect.top <= 100 && rect.bottom >= 100;
-        }
-        return false;
-      });
-      setActiveSection(currentSection || "");
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const [ctaClosed, setCtaClosed] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 1000) {
@@ -133,13 +115,13 @@ export function App() {
     {
       href: "https://github.com/bobbygrdn",
       label: "GitHub",
-      icon: <Github className="h-5 w-5" />,
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>,
       username: "@bobbygrdn",
     },
     {
       href: "https://linkedin.com/in/bobbygrdn",
       label: "LinkedIn",
-      icon: <Linkedin className="h-5 w-5" />,
+      icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>,
       username: "Full Stack Engineer",
     },
   ];
@@ -162,14 +144,10 @@ export function App() {
                 key={href}
                 href={href}
                 onClick={(e) => scrollToSection(e, href)}
-                className={`group flex items-center space-x-2 text-sm lg:text-base transition-colors relative ${activeSection === href.slice(1) ? "text-blue-400" : "text-zinc-400 hover:text-zinc-100"}`}
+                className={`group flex items-center space-x-2 text-sm lg:text-base transition-colors relative text-zinc-400 hover:text-zinc-100`}
               >
                 {icon}
                 <span>{label}</span>
-                {activeSection === href.slice(1) && (
-                  <span className="absolute -bottom-[1.6rem] left-0 w-full h-0.5 bg-gradient-to-r from-blue-400 to-cyan-400" />
-                )}
-                <span className="absolute -bottom-[1.6rem] left-0 w-full h-0.5 bg-gradient-to-r from-blue-400 to-cyan-400 scale-x-0 group-hover:scale-x-100 transition-transform" />
               </a>
             ))}
           </div>
@@ -193,7 +171,7 @@ export function App() {
                   key={href}
                   href={href}
                   onClick={(e) => scrollToSection(e, href)}
-                  className={`flex items-center space-x-3 p-3 rounded-lg transition-colors ${activeSection === href.slice(1) ? "text-blue-400 bg-blue-400/10" : "text-zinc-400 hover:text-zinc-100 hover:bg-zinc-800"}`}
+                  className={`flex items-center space-x-3 p-3 rounded-lg transition-colors`}
                 >
                   {icon}
                   <span className="text-base">{label}</span>
@@ -329,71 +307,7 @@ export function App() {
                     </div>
                   </div>
                 ))}
-              {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 sm:gap-8">
-                {projects
-                  .filter((p) => !p.featured)
-                  .map((project) => (
-                    <div
-                      key={project.id}
-                      className="group relative bg-zinc-900 border border-zinc-800 rounded-xl overflow-hidden hover:border-blue-500/50 transition-all duration-500 hover:shadow-[0_0_30px_rgba(59,130,246,0.15)]"
-                    >
-                      <div className="h-48 relative overflow-hidden">
-                        {isImageLoading[project.id] && (
-                          <div className="absolute inset-0 bg-zinc-900 animate-pulse" />
-                        )}
-                        <img
-                          src={project.image}
-                          alt={project.title}
-                          className="w-full h-full object-cover transition-all duration-500 group-hover:scale-105"
-                          onLoad={() =>
-                            setIsImageLoading((prev) => ({
-                              ...prev,
-                              [project.id]: false,
-                            }))
-                          }
-                        />
-                        <div className="absolute inset-0 bg-gradient-to-t from-zinc-900/60 to-transparent" />
-                      </div>
-                      <div className="p-6">
-                        <div className="flex items-center justify-between mb-4">
-                          <span className="text-sm text-blue-400">
-                            {project.category}
-                          </span>
-                          <span
-                            className={`px-2 py-1 rounded-full text-xs ${project.status === "Live" ? "bg-green-400/10 text-green-400" : project.status === "In Development" ? "bg-yellow-400/10 text-yellow-400" : "bg-zinc-400/10 text-zinc-400"}`}
-                          >
-                            {project.status}
-                          </span>
-                        </div>
-                        <h3 className="text-xl font-bold mb-3">
-                          {project.title}
-                        </h3>
-                        <p className="text-zinc-400 mb-6 text-sm">
-                          {project.description}
-                        </p>
-                        <div className="space-y-4">
-                          <div className="flex flex-wrap items-center gap-2">
-                            {project.tech.map((tech) => (
-                              <span
-                                key={tech}
-                                className="px-2 py-1 bg-zinc-800/50 text-zinc-300 rounded-full text-xs border border-zinc-700"
-                              >
-                                {tech}
-                              </span>
-                            ))}
-                          </div>
-                          <a
-                            href={project.link}
-                            className="inline-flex items-center text-blue-400 hover:text-blue-300 transition-colors"
-                          >
-                            View Project{" "}
-                            <ExternalLink className="ml-2 h-4 w-4" />
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-              </div> */}
+
             </div>
           </div>
         </section>
@@ -455,8 +369,10 @@ export function App() {
         </footer>
       </main>
       <FloatingCTA
-        isVisible={showFloatingCTA}
-        onClose={() => setShowFloatingCTA(false)}
+        isVisible={showFloatingCTA && !ctaClosed}
+        onClose={() => {
+          setCtaClosed(true);
+        }}
       />
     </div>
   );
